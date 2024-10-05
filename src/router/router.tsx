95d0013c50fc02f20bb0bab032680cf91f53c08b
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:03:37 +0300

    "Added Home and Transactions routes to router, and a placeholder for categories route"

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+import Transactions from "../pages/Transactions";
+import Categories from "../pages/Categories";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "transactions", element: <Transactions /> },
+      { path: "categories", element: <Categories /> },
+    ],
   },
-  {},
 ]);
