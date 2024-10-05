8bde637dca1d3e911ef5b2e1bfc1a01572fd80ad
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:14:24 +0300

    "Added FaBtc icon to Header component and replaced placeholder with actual Header component in Layout page"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-
+import { FaBtc } from 'react-icons/fa'
 const Header: FC = () => {
     const isAuth = true
     return (
         <header className="flex items-center justify-between bg-slate-800 px-4 py-2 shadow-sm backdrop-blur-sm">
-            <Link to={'/'} />
+            <Link to={'/'}>
+                <FaBtc size={20} />
+            </Link>
         </header>
     )
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 const Layout: FC = () => {
     return (
         <div className="min-h-screen bg-slate-900 font-roboto text-white pb-20">
-            <div>Header</div>
+            <div>
+                <Header />
+            </div>
             <div className="container">
                 <Outlet />
             </div>
