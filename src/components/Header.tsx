1863618450deebc000cfc1a642509b8da5028817
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:10:53 +0300

    "Updated Header component to use parentheses for JSX return statement"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 
 const Header: FC = () => {
-    return <header className='flex items-center justify-between bg-slate-800 px-4 py-2 shadow-sm backdrop-blur-sm'></header>
+    return (
+        <header className="flex items-center justify-between bg-slate-800 px-4 py-2 shadow-sm backdrop-blur-sm"></header>
+    )
 }
 
 export default Header
