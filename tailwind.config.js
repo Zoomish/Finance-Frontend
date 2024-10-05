59234bda668453de04ef35c4567fe97926206e87
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 18:02:45 +0300

    "Updated tailwind.config.js to add font family and color extensions to the theme"

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto','sans-serif'],
+      },
+      colors: {
+        'primary': '#3490dc',
+        'dark': '#212121',
+      },
+    },
   },
   plugins: [],
 }
