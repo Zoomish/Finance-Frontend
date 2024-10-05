ecd0f713a03c85f64443e44b88fef073947b22b7
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:02:13 +0300

    init

---
+++ src/router/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Layout";
+import ErrorPage from "../pages/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+  },
+  {},
+]);
