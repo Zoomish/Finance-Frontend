487ada2b18753c521c8aaafb70f4b8a13705a7ff
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:11:51 +0300

    "Added Link component from react-router-dom to Header component"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 const Header: FC = () => {
+    const isAuth = true
     return (
-        <header className="flex items-center justify-between bg-slate-800 px-4 py-2 shadow-sm backdrop-blur-sm"></header>
+        <header className="flex items-center justify-between bg-slate-800 px-4 py-2 shadow-sm backdrop-blur-sm">
+            <Link to={'/'} />
+        </header>
     )
 }
 
