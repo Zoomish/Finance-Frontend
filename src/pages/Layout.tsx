1119ac0f368bdfa95b7a492048714a44cc317cd4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 18:54:59 +0300

    "Added Outlet component from react-router-dom to Layout component"

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout: FC = () => {
   return (
     <div className="min-h-screen bg-slate-900 font-roboto text-white pb-20">
       <div>Header</div>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   );
 };
