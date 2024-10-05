7d3898398d6f90ad435a579ddeee52e9b2864d20
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:18:43 +0300

    Remove unnecessary div wrapper around Header component.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,9 +5,7 @@ import Header from '../components/Header'
 const Layout: FC = () => {
     return (
         <div className="min-h-screen bg-slate-900 font-roboto text-white pb-20">
-            <div>
-                <Header />
-            </div>
+            <Header />
             <div className="container">
                 <Outlet />
             </div>
