9db5cbd32c6fe7dabe0bea6f9d6a33595a740d86
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 19:05:14 +0300

    "Updated code formatting and quotes in various configuration files"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
-      padding: '2rem',
+      padding: "2rem",
       center: true,
     },
     extend: {
       fontFamily: {
-        'roboto': ['Roboto', 'sans-serif'],
+        roboto: ["Roboto", "sans-serif"],
       },
       colors: {
-        'primary': '#3490dc',
-        'dark': '#212121',
+        primary: "#3490dc",
+        dark: "#212121",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'), "prettier-plugin-tailwindcss"],
-}
+  plugins: [require("@tailwindcss/forms"), "prettier-plugin-tailwindcss"],
+};
