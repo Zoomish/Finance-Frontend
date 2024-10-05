dd26bb801e89594bd456f2a6a2a395faa2040ab0
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-05 18:03:17 +0300

    "Added container config to theme and updated font family in tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: '2rem',
+      center: true,
+    },
     extend: {
       fontFamily: {
-        'roboto': ['Roboto','sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
       },
       colors: {
         'primary': '#3490dc',
