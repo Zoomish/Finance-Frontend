f2be1f2104c1f73d8ad8732f03cac7e78a446ae2
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 20:56:54 +0300

    "Updated axios API to use getTokenFromLocalStorage instead of VITE_BASEAPI_TOKEN"

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
+import { getTokenFromLocalStorage } from '../helper/localStorage.helper'
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_BASEAPI_URL,
     headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_BASEAPI_TOKEN}`,
-    }
+        Authorization: `Bearer ${getTokenFromLocalStorage()}`,
+    },
 })
