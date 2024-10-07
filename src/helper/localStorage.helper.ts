ef5b4b75519e141adbfdd6c07d638358bd6e3a8e
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 20:55:04 +0300

    "Updated getTokenFromLocalStorage function to return string instead of void, and parse JSON data from localStorage"

--- src/helper/localStorage.helper.ts
+++ src/helper/localStorage.helper.ts
@@ -1,3 +1,4 @@
-export function getTokenFromLocalStorage(): void {
-    const token = localStorage.getItem('token')
+export function getTokenFromLocalStorage(): string {
+    const data = localStorage.getItem('token')
+    return data ? JSON.parse(data) : ''
 }
