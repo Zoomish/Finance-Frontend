3a872e15cef51ce1425c81ef2f0f028e968652e9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 20:52:59 +0300

    "Added Authorization header with Bearer token to axios API instance"

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_BASEAPI_URL,
+    headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_BASEAPI_TOKEN}`,
+    }
 })
