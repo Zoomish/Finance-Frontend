8779ab79460fda1e6636ca09127aed9993c2f6f0
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 20:55:57 +0300

    "Added new function setKeyToLocalStorage to localStorage.helper.ts"

--- src/helper/localStorage.helper.ts
+++ src/helper/localStorage.helper.ts
@@ -2,3 +2,7 @@ export function getTokenFromLocalStorage(): string {
     const data = localStorage.getItem('token')
     return data ? JSON.parse(data) : ''
 }
+
+export function setKeyToLocalStorage(key: string, token: string): void {
+    localStorage.setItem(key, JSON.stringify(token))
+}
