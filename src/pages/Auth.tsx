f9ba6e318603f85691330f026b6377ac972c95c9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 20:25:53 +0300

    "Added useState import and initialized isLogin state variable in Auth component"

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 const Auth: FC = () => {
+    const [isLogin, setIsLogin] = useState<boolean>(true)
     return (
         <div>
             <h1>Login</h1>
