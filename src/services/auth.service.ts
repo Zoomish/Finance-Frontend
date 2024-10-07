7fa067bb82570093d88e032844552101087bba5c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-07 21:11:40 +0300

    "Added implementation for registration method in AuthService to make a POST request to 'user' endpoint using axios api"

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,14 @@
+import { api } from '../api/axios.api'
 import { IAuthResponse, IUserData } from '../types/Auth'
 
 export const AuthService = {
     async registration(
         userData: IUserData
-    ): Promise<IAuthResponse | undefined> {},
+    ): Promise<IAuthResponse | undefined> {
+        const { data } = await api.post<IAuthResponse>('user', userData)
+
+        return data
+    },
     async login() {},
     async getMe() {},
 }
