5f55c768d6f92e4deb03905b0ac535a2d837da83
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-13 17:26:32 +0300

    "Wrap App component with react-redux Provider and StrictMode"

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 import App from './App.tsx'
 import './index.css'
-import { ToastContainer } from 'react-toastify'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-        <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            newestOnTop={true}
-            limit={3}
-        />
-    </StrictMode>
+    <Provider store={store}>
+        <StrictMode>
+            <App />
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                newestOnTop={true}
+                limit={3}
+            />
+        </StrictMode>
+    </Provider>
 )
