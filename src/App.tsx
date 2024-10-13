e110db65de8fc3786ef0f32141e5b8cc43c28583
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-13 18:22:39 +0300

    "Added imports and code for token retrieval and authentication check in App component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router'
+import { useAppDispatch } from './redux/hooks'
+import { getTokenFromLocalStorage } from './helper/localStorage.helper'
 
 function App() {
+    const dispatch = useAppDispatch()
+    const checkAuth = async () => {
+        const token = getTokenFromLocalStorage()
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return <RouterProvider router={router} />
 }
 
