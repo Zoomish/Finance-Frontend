1ba81d52b9d98eddcd90f00b73a92f28e6a23477
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-13 17:58:10 +0300

    Added import statement for React Toastify CSS file.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App.tsx'
 import './index.css'
 import { store } from './redux/store.ts'
