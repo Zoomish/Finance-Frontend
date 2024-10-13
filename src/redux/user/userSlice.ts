982e0805fb316988b190155c00805f655dcbb3d6
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-13 15:15:20 +0300

    "Refactor userSlice: rename state and actions, update types and initial state"

--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -1,32 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
+import { IUser } from '../../types/User'
 
-interface CounterState {
-    value: number
+interface IUserState {
+    user: IUser
 }
 
-const initialState: CounterState = {
-    value: 0,
+const initialState: IUserState = {
+    user: {} as IUser,
 }
 
 export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action: PayloadAction<number>) => {
-            state.value += action.payload
+        setUser: (state, action: PayloadAction<IUser>) => {
+            state.user = action.payload
         },
     },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const {} = counterSlice.actions
 
 export const selectCount = (state: RootState) => state.counter.value
 
