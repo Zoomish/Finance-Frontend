b93e81c8eee1c8a49ea5cfb99054514ac783a882
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 17:14:25 +0300

    init

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface Props {
+    children: JSX.Element
+}
+
+const ProtectedRoute: FC<Props> = ({ children }) => {
+    return <div></div>
+}
+
+export default ProtectedRoute
