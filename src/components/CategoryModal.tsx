3695a43a9820f2d00aea3ddbeb9851da21a01928
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 21:00:12 +0300

    "Added form fields and buttons to CategoryModal component"

--- src/components/CategoryModal.tsx
+++ src/components/CategoryModal.tsx
@@ -14,7 +14,19 @@ const CategoryModal: FC<ICategoryModalProps> = ({
 }) => {
     return (
         <div className="fixed top-0 right-0 left-0 bottom-0 w-full h-full bg-black/50 flex justify-center items-center">
-            <Form></Form>
+            <Form className="">
+                <label htmlFor="">
+                    <small>Label</small>
+                    <input type="text" />
+                </label>
+
+                <div className="flex items-center gap-2">
+                    <button type="submit">Submit</button>
+                    <button onClick={() => setVisibleModal(false)}>
+                        Cancel
+                    </button>
+                </div>
+            </Form>
         </div>
     )
 }
