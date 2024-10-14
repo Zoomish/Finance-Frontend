99f9c5cad664719c22a33ae181713c76083e580c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 17:17:37 +0300

    "Update ProtectedRoute component to display a styled message when user is not authenticated"

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,7 +7,17 @@ interface Props {
 
 const ProtectedRoute: FC<Props> = ({ children }) => {
     const isAuth = useAuth()
-    return <>{isAuth ? children : <div>Yuu dont enter</div>}</>
+    return (
+        <>
+            {isAuth ? (
+                children
+            ) : (
+                <div className="mt-20 flex flex-col items-center justify-center gap-10">
+                    <h1 className="text-2xl">You are not logged in</h1>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default ProtectedRoute
