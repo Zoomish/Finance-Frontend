2498190e7baff247ab341fc77549a05f60346c32
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 20:34:41 +0300

    "Refactor Categories component: reorganize layout, add CategoryModal, and adjust button and form elements"

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,36 +2,40 @@ import { FC } from 'react'
 import { AiFillCloseCircle, AiFillEdit } from 'react-icons/ai'
 import { FaPlus } from 'react-icons/fa'
 import { Form } from 'react-router-dom'
+import CategoryModal from '../components/CategoryModal'
 
 const Categories: FC = () => {
     return (
-        <div className="mt-10 p-4 rounded-md bg-slate-400">
-            <h1>Your category list</h1>
-            <div className="flex mt-2 items-center gap-2 flex-wrap">
-                <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
-                    Salary
-                    <div className="absolute hidden rounded-lg px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between group-hover:flex">
-                        <button>
-                            <AiFillEdit />
-                        </button>
-                        <Form
-                            className="flex"
-                            method="delete"
-                            action="/categories"
-                        >
-                            <input type="hidden" value={'Category ID'} />
-                            <button type="submit">
-                                <AiFillCloseCircle />
+        <>
+            <div className="mt-10 p-4 rounded-md bg-slate-400">
+                <h1>Your category list</h1>
+                <div className="flex mt-2 items-center gap-2 flex-wrap">
+                    <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
+                        Salary
+                        <div className="absolute hidden rounded-lg px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between group-hover:flex">
+                            <button>
+                                <AiFillEdit />
                             </button>
-                        </Form>
+                            <Form
+                                className="flex"
+                                method="delete"
+                                action="/categories"
+                            >
+                                <input type="hidden" value={'Category ID'} />
+                                <button type="submit">
+                                    <AiFillCloseCircle />
+                                </button>
+                            </Form>
+                        </div>
                     </div>
                 </div>
+                <button className="mt-5 max-w-fit flex items-center gap-2 text-white/50 hover:text-white">
+                    <FaPlus />
+                    <span>Add new category</span>
+                </button>
             </div>
-            <button className="mt-5 max-w-fit flex items-center gap-2 text-white/50 hover:text-white">
-                <FaPlus />
-                <span>Add new category</span>
-            </button>
-        </div>
+            <CategoryModal type="post" id={0} setVisibleModal={(setVisibleModal)} />
+        </>
     )
 }
 
