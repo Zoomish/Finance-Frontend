6753e0a4f1215bea9016773190fc30f4cafb3846
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 18:52:35 +0300

    Added a new div element with absolute positioning inside the existing div with a blue background.

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -5,7 +5,9 @@ const Categories: FC = () => {
         <div className="mt-10 p-4 rounded-md bg-slate-400">
             <h1>Your category list</h1>
             <div className="flex mt-2 items-center gap-2 flex-wrap">
-                <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2"></div>
+                <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
+                    <div className="absolute px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between"></div>
+                </div>
             </div>
         </div>
     )
