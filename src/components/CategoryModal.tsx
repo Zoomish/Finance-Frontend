69a38d09ff5083337cb4aa98ad2a27633a05212b
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 20:37:14 +0300

    "Added Form component from react-router-dom to CategoryModal"

--- src/components/CategoryModal.tsx
+++ src/components/CategoryModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { Form } from 'react-router-dom'
 
 interface ICategoryModalProps {
     type: 'post' | 'patch'
@@ -12,7 +13,9 @@ const CategoryModal: FC<ICategoryModalProps> = ({
     setVisibleModal,
 }) => {
     return (
-        <div className="fixed top-0 right-0 left-0 bottom-0 w-full h-full bg-black/50 flex justify-center items-center"></div>
+        <div className="fixed top-0 right-0 left-0 bottom-0 w-full h-full bg-black/50 flex justify-center items-center">
+            <Form></Form>
+        </div>
     )
 }
 
