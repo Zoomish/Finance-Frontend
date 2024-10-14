e72cd914ee052febde4a67975babbac52b9d97f0
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 18:53:06 +0300

    "Added AiFillEdit icon from react-icons/ai and wrapped it in a button within an absolute div in the Categories component."

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { AiFillEdit } from 'react-icons/ai'
 
 const Categories: FC = () => {
     return (
@@ -6,7 +7,11 @@ const Categories: FC = () => {
             <h1>Your category list</h1>
             <div className="flex mt-2 items-center gap-2 flex-wrap">
                 <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
-                    <div className="absolute px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between"></div>
+                    <div className="absolute px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between">
+                        <button>
+                            <AiFillEdit />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
