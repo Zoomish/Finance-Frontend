4189f2d177d197a87810a491a8938b210a76a07a
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 18:38:09 +0300

    "Updated Categories component with new div element and added placeholder for category list rendering"

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,8 +2,11 @@ import { FC } from 'react'
 
 const Categories: FC = () => {
     return (
-        <div className='mt-10 p-4 rounded-md bg-slate-400'>
+        <div className="mt-10 p-4 rounded-md bg-slate-400">
             <h1>Your category list</h1>
+            <div className="flex mt-2 items-center gap-2 flex-wrap">
+                {/* Render list of categories */}
+            </div>
         </div>
     )
 }
