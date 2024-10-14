e80ee9dd3fb1bdf758de7d64e34c5571d31d045d
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 17:16:11 +0300

    "Added useAuth hook to ProtectedRoute component and conditional rendering of children based on auth status"

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
+import { useAuth } from '../hooks/useAuth'
 
 interface Props {
     children: JSX.Element
 }
 
 const ProtectedRoute: FC<Props> = ({ children }) => {
-    return <div></div>
+    const isAuth = useAuth()
+    return <>{isAuth ? children : <div>Yuu dont enter</div>}</>
 }
 
 export default ProtectedRoute
