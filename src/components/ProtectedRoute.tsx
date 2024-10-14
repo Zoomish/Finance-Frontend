b8a75b3e601fb0995e0d4ab59a816a5f1d51cce6
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 17:20:28 +0300

    "Added image and login button to unauthenticated route message"

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -13,7 +13,14 @@ const ProtectedRoute: FC<Props> = ({ children }) => {
                 children
             ) : (
                 <div className="mt-20 flex flex-col items-center justify-center gap-10">
+                    <img src="" alt="You are not logged in" />
                     <h1 className="text-2xl">You are not logged in</h1>
+                    <button
+                        onClick={() => (window.location.href = '/login')}
+                        className="btn btn-green"
+                    >
+                        Log in
+                    </button>
                 </div>
             )}
         </>
