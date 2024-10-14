968970a64cd0492721be4168543dbde10cf4fbb3
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 19:17:19 +0300

    Added "Salary" text inside a div element in Categories.tsx.

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -7,6 +7,7 @@ const Categories: FC = () => {
             <h1>Your category list</h1>
             <div className="flex mt-2 items-center gap-2 flex-wrap">
                 <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
+                    Salary
                     <div className="absolute px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between">
                         <button>
                             <AiFillEdit />
