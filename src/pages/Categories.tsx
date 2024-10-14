f3e81edbe37db1adda93e2c8597c2309e0eeca4c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-14 18:51:21 +0300

    Added `gap-2` class to inner `div` element in Categories component.

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -5,7 +5,7 @@ const Categories: FC = () => {
         <div className="mt-10 p-4 rounded-md bg-slate-400">
             <h1>Your category list</h1>
             <div className="flex mt-2 items-center gap-2 flex-wrap">
-                <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative"></div>
+                <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2"></div>
             </div>
         </div>
     )
