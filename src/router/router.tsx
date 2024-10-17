9909d489cc9e5bdf62ba83a44eca264896fadbd5
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-17 17:55:23 +0300

    "Added categoryLoader to Categories route in router"

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import ProtectedRoute from '../components/ProtectedRoute'
 import Auth from '../pages/Auth'
-import Categories, { categoriesAction } from '../pages/Categories'
+import Categories, {
+    categoriesAction,
+    categoryLoader,
+} from '../pages/Categories'
 import ErrorPage from '../pages/ErrorPage'
 import Home from '../pages/Home'
 import Layout from '../pages/Layout'
@@ -25,6 +28,7 @@ export const router = createBrowserRouter([
             {
                 path: 'categories',
                 action: categoriesAction,
+                loader: categoryLoader,
                 element: (
                     <ProtectedRoute>
                         <Categories />
