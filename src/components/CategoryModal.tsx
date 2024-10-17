39f7fbf7666c5bddfc90729b0a1b1889c5aefcbd
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-17 18:31:09 +0300

    "Added 'id' prop to CategoryModal component and updated destructuring in function signature"

--- src/components/CategoryModal.tsx
+++ src/components/CategoryModal.tsx
@@ -3,10 +3,15 @@ import { Form } from 'react-router-dom'
 
 interface ICategoryModalProps {
     type: 'post' | 'patch'
+    id?: number
     setVisibleModal: (visible: boolean) => void
 }
 
-const CategoryModal: FC<ICategoryModalProps> = ({ type, setVisibleModal }) => {
+const CategoryModal: FC<ICategoryModalProps> = ({
+    type,
+    id,
+    setVisibleModal,
+}) => {
     return (
         <div className="fixed top-0 right-0 left-0 bottom-0 w-full h-full bg-black/50 flex justify-center items-center">
             <Form
