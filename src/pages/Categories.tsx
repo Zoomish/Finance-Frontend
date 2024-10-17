a1d110f0761e371c08b07200ac1df3a5ffb9aaf3
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-17 18:16:09 +0300

    Added a colon to the "Your category list" heading.

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -45,7 +45,7 @@ const Categories: FC = () => {
     return (
         <>
             <div className="mt-10 p-4 rounded-md bg-slate-400">
-                <h1>Your category list</h1>
+                <h1>Your category list: </h1>
                 <div className="flex mt-2 items-center gap-2 flex-wrap">
                     {categories.map((category) => (
                         <div
