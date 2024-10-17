e4f18b1f6d67eeac77488bfef114b06f7765ae53
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-17 18:15:58 +0300

    "Updated categoryLoader to specify return type, and refactored category list rendering to use map function"

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -35,7 +35,7 @@ export const categoriesAction = async ({ request }: any) => {
 }
 
 export const categoryLoader = async () => {
-    const { data } = await api.get('/category')
+    const { data } = await api.get<ICategory[]>('/category')
     return data
 }
 
@@ -47,24 +47,32 @@ const Categories: FC = () => {
             <div className="mt-10 p-4 rounded-md bg-slate-400">
                 <h1>Your category list</h1>
                 <div className="flex mt-2 items-center gap-2 flex-wrap">
-                    <div className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2">
-                        Salary
-                        <div className="absolute hidden rounded-lg px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between group-hover:flex">
-                            <button>
-                                <AiFillEdit />
-                            </button>
-                            <Form
-                                className="flex"
-                                method="delete"
-                                action="/categories"
-                            >
-                                <input type="hidden" value={'Category ID'} />
-                                <button type="submit">
-                                    <AiFillCloseCircle />
+                    {categories.map((category) => (
+                        <div
+                            key={category.id}
+                            className="group py-2 px-4 rounded-lg bg-blue-600 flex items-center relative gap-2"
+                        >
+                            Salary
+                            <div className="absolute hidden rounded-lg px-3 left-0 top-0 bottom-0 right-0 bg-black/90 items-center justify-between group-hover:flex">
+                                <button>
+                                    <AiFillEdit />
                                 </button>
-                            </Form>
+                                <Form
+                                    className="flex"
+                                    method="delete"
+                                    action="/categories"
+                                >
+                                    <input
+                                        type="hidden"
+                                        value={'Category ID'}
+                                    />
+                                    <button type="submit">
+                                        <AiFillCloseCircle />
+                                    </button>
+                                </Form>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
                 <button
                     onClick={() => setVisibleModal(true)}
